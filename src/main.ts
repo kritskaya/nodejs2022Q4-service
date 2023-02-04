381a8f89e4367b1e92e9f77996a46cd3f287c0a2
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2023-02-04 14:42:46 +0300

    feat: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = await readFile(join(cwd(), 'doc', 'api.yaml'), 'utf8');
+  const document = parse(file);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
